668212d5a84e6c240b394f52eca3d08db98c83f5
Sachin Iyer <sachinjiyer@gmail.com>
2024-09-07 17:48:38 -0700

    clean up the python

--- python.py
+++ python.py
@@ -9,22 +9,14 @@ import os
 import random
 
 
-def get_hello_value():
-    """
-    Return a string.
-
-    Returns a string from the Rust DLL.
-    """
-
-
 dll = ctypes.CDLL("./target/release/librust_dll.so")
-dll.hello.restype = ctypes.c_char_p
 
 if os.name == "nt":
     dll = ctypes.CDLL("./target/release/rust_dll.dll")
 elif os.name == "darwin":
     dll = ctypes.CDLL("./target/release/librust_dll.dylib")
 
+dll.hello.restype = ctypes.c_char_p
 
 x = random.randint(0, 100)
 y = random.randint(0, 100)
